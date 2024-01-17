982f1073426c9378ed6cb1b6747f5b091d38d492
Cmorales <moralesrojascr@gmail.com>
2024-01-17 21:14:36 +0100

    Give a description of the instalation of each container

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
+# => Start mysql service
 service mysql start;
 
+# => Give a delay for a good service init
 sleep 10
 
+# => Change the password of the root user
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
+# => Create a database (if the database does not exist)
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
+# => Create an user with a password (if the user does not exist)
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+# => Give all privileges to the user
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+# => Delete the user '' if exits
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "DROP USER IF EXISTS ''@'localhost';"
 
+# => Reload privileges after making changes
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
+# => Turn off the service
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
+# => Start mysql service safety mode
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# => Give a delay to wait mariadb configure right
 sleep 10
 
+# => update (with sudo) cli if is necessary
 wp-cli.phar cli update --yes --allow-root
+# => Downloads the core (main fails) of wordpress
 wp-cli.phar core download --allow-root
 
-
+# => Configure the database partner with mariadb 
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp-cli.phar config create --allow-root \
         --dbname=$SQL_DATABASE \
@@ -14,8 +17,10 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
         --dbhost=mariadb:3306 \
         --path='/var/www/wordpress'
 
+    # => Give a delay to configure right
     sleep 2
 
+    # => Install and configure the core (main fails) of wordpress
     wp-cli.phar core install --allow-root \
     	--url=$DOMAIN_NAME \
         --title=$SITE_TITLE \
@@ -24,14 +29,17 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
         --admin_email=$ADMIN_EMAIL \
         --path='/var/www/wordpress'
 
+    # => Create a user with cli
     wp-cli.phar user create --allow-root \
         --role=author $USER1_LOGIN $USER1_MAIL \
         --user_pass=$USER1_PASS \
         --path='/var/www/wordpress'
 fi
 
+# => Check if exist this directory,else creat taht is necesario for execute php
 if [ ! -d /run/php ]; then
     mkdir ./run/php
 fi
 
+# =>Init the service php int first map
 /usr/sbin/php-fpm7.3 -F
